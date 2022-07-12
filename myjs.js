df29d71aac7c88a062eded58287dfedab44d3327
itosubaru <103973953+itosubaru@users.noreply.github.com>
2022-07-12 10:23:06 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -4,4 +4,9 @@
 /* alert'(Math.sqrt(2)); */
 /* alert(Math.sqrt(2+3)); */
 /* alert(1/0); */
-alert(0/0);
+/* alert(0/0); */
+alert(2+3);
+alert('2'+'3');
+alert(Number('2')+Number('3'));
+alert('2'+3);
+alert(Number('2')+3);
