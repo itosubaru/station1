1979e46b796e86426cfa7392a9362aa7221c198c
itosubaru <103973953+itosubaru@users.noreply.github.com>
2022-07-16 21:16:44 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,12 +1,12 @@
-/* alert('Hello, Subaru!'); */
-/* alert(1+1); */
-/* alert(Math.sqrt(1+1)); */
-/* alert'(Math.sqrt(2)); */
-/* alert(Math.sqrt(2+3)); */
-/* alert(1/0); */
-/* alert(0/0); */
-/* alert(2+3); */
-/* alert('2'+'3'); */
-/* alert(Number('2')+Number('3')); */
-/* alert('2'+3); */
-/* alert(Number('2')+3); */
+alert('Hello, Subaru!');
+alert(1+1);
+alert(Math.sqrt(1+1));
+alert'(Math.sqrt(2));
+alert(Math.sqrt(2+3));
+alert(1/0);
+alert(0/0);
+alert(2+3);
+alert('2'+'3');
+alert(Number('2')+Number('3'));
+alert('2'+3);
+alert(Number('2')+3);
