5fc585604d2f89aff896b83528a4463f5838373e
itosubaru <103973953+itosubaru@users.noreply.github.com>
2022-07-17 20:55:58 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,5 +1,5 @@
 alert('Hello, Subaru!');
-// alert(1+1);
+/* alert(1+1);
 alert(Math.sqrt(1+1));
 alert'(Math.sqrt(2));
 alert(Math.sqrt(2+3));
@@ -9,4 +9,4 @@ alert(2+3);
 alert('2'+'3');
 alert(Number('2')+Number('3'));
 alert('2'+3);
-alert(Number('2')+3); //
+alert(Number('2')+3); */
